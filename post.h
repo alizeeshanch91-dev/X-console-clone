c253e0f12c5fbff9baff4bf157febf528ab13563
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-07-31 23:38:19 +0500

    added all basic structure in post.h

--- post.h
+++ post.h
@@ -3,12 +3,36 @@
 
 using namespace std;
 
-class post
+class Post
 {
 private:
+	int authorIndex;
 	string content;
 	int views;
-	int likes;
-	int comments;
-	string comment;
+
+	int* likedBy;
+	int likedByCount;
+	int linkedByCapacity;
+	void resizeLikedBy();
+
+	string* comments;
+	int commentCounts;
+	int commentCapacity;
+	void resizeComments();
+public:
+	Post(int authorIndex, string content);
+	~Post();
+	Post(const Post& other);
+	Post& operator=(const Post& other);
+	void like(int userIndex);
+	bool hasLiked(int userIndex) const;
+	void addView();
+	void addComment(const string& commentyText);
+	//getters
+	int getAuthorIndex() const;
+	string getContent() const;
+	int getViews() const;
+	int getLikeCount() const;
+	int getCommentCount() const;
+	string getComment(int index) const;
 };
